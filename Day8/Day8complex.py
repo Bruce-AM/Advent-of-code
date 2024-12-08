4f2771c106353ed382129183827edde7970e3d90
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-08 16:48:23 +0200

    Day8complex.py

--- Day8/Day8complex.py
+++ Day8/Day8complex.py
@@ -18,17 +18,12 @@ def both_parts():
             part2_antidot.add(a)
             part2_antidot.add(b)
             d = b-a
-            ad, bd = a-d, b+d
-            if (-1 < ad.real < rows) and (-1 < ad.imag < cols):
-                part1_antidot.add(ad)
-                while (-1 < ad.real < rows) and (-1 < ad.imag < cols): #y.row.real x.col.imag
-                    part2_antidot.add(ad)
-                    ad -= d
-            if (-1 < bd.real < rows) and (-1 < bd.imag < cols):
-                part1_antidot.add(bd)
-                while (-1 < bd.real < rows) and (-1 < bd.imag < cols):
-                    part2_antidot.add(bd)
-                    bd += d
+            for pos, dis in ((a-d,-d), (b+d,d)):
+                if (-1 < pos.real < rows) and (-1 < pos.imag < cols):
+                    part1_antidot.add(pos)
+                    while (-1 < pos.real < rows) and (-1 < pos.imag < cols):
+                        part2_antidot.add(pos)
+                        pos += dis
     print(len(part1_antidot), len(part2_antidot.union(part1_antidot)))
   
 both_parts()
