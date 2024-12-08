f61beb045f1b94b6cede94fc115eb4f2c1307b5a
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-08 16:50:16 +0200

    Day8complex.py

--- Day8/Day8complex.py
+++ Day8/Day8complex.py
@@ -15,8 +15,7 @@ def both_parts():
     part2_antidot = set()
     for freq in frequencies.values():
         for a, b in combinations(freq, 2):            
-            part2_antidot.add(a)
-            part2_antidot.add(b)
+            [part2_antidot.add(n) for n in (a, b)]
             d = b-a
             for pos, dis in ((a-d,-d), (b+d,d)):
                 if (-1 < pos.real < rows) and (-1 < pos.imag < cols):
