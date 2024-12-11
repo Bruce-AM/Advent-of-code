2574edf97feb8315b5c9c0e3c28853580fa4380b
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-11 22:24:10 +0200

    Day11.py

--- Day11.py
+++ Day11.py
@@ -1,45 +1,18 @@
-import timeit
 from collections import defaultdict 
-import math
+import timeit
 
-def part():
-    with open(r"day11_input.txt", "r") as file:
-        stones = file.readline().split()
-    
-    numbers = defaultdict(int)
-    for num in stones:
-        numbers[num] += 1
-    
-    for _ in range(75):
-        new_numbers = defaultdict(int)
-        for num, amount in numbers.items():
-            if len(num) % 2:
-                n = int(num)
-                if n:
-                    new_numbers[str(n*2024)] += amount
-                else:
-                    new_numbers['1'] += amount
-            else:
-                half = len(num)//2
-                new_numbers[num[:half]] += amount
-                new_numbers[str(int(num[half:]))] += amount
-        numbers = new_numbers
-    print(sum(numbers.values()))
-
-#or
-def part_v2():
+def both():
     with open(r"day11_input.txt", "r") as file:
         stones = map(int, file.readline().split())
     
     numbers = defaultdict(int)
-    for num in stones:
-        numbers[num] += 1
+    [numbers[num] += 1 for num in stones]
 
-    for _ in range(75):
+    for _ in range(75): # print(sum(numbers.values())) if _ == 25
         new_numbers = defaultdict(int)
         for num, amount in numbers.items():
             if num:
-                length = math.floor(math.log10(num))+1
+                length = len(str(num))
                 if length % 2:
                     new_numbers[num * 2024] += amount
                 else:
@@ -51,4 +24,4 @@ def part_v2():
         numbers = new_numbers
     print(sum(numbers.values()))
 
-part_v2()
+both()
