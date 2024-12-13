11c0ff2d5e6d83201eecfb327ac01715c3f97546
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-13 23:12:47 +0200

    Day13.py

--- Day13.py
+++ Day13.py
@@ -1,23 +1,16 @@
 import re
 
 with open(r"day13_input.txt", "r") as file:
-    a = file.readlines()
-
-claw_machines = []
-cur_machine = []
-for string in a:
-    if string != '\n':
-        cur_machine.append((tuple(map(int, re.findall(r'[-+]?\d+', string)))))
-    else:
-        claw_machines.append(cur_machine)
-        cur_machine = []
-claw_machines.append(cur_machine)
+    a = ''.join(file.read().split('\n\n'))
+claw_machines = list(map(int, re.findall(r'[-+]?\d+', a)))
+length = len(claw_machines)
 
 def part1(p2):
     tokens = 0
-    for a, b, prize in claw_machines:
-        b_times = (a[0] * (prize[1]+p2) - a[1] * (prize[0]+p2)) / (a[0] * b[1] - a[1] * b[0])
-        a_times = ((prize[0]+p2) - (b[0] * b_times)) / a[0]
+    for i in range(0, length, 6):
+        ax,ay, bx,by, prizex,prizey = claw_machines[i:i+6]
+        b_times = (ax * (prizey+p2) - ay * (prizex+p2)) / (ax * by - ay * bx)
+        a_times = ((prizex+p2) - (bx * b_times)) / ax
         if b_times == int(b_times) and a_times == int(a_times):         
             tokens +=  a_times * 3 + b_times
     print(int(tokens))
