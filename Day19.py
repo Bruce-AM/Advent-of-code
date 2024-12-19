782961d4f5752e84e7fd0a58be9e0c42dfc537a0
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-19 16:36:51 +0200

    Update Day19.py

--- Day19.py
+++ Day19.py
@@ -5,33 +5,19 @@ with open("adventofcode/day19/day19_input.txt","r") as file:
 towels = tow.strip().split(", ")
 patterns = pat.split("\n")
 
-def part1():
+def different_ways(p):
+    cache = {}
     def dfs(p):
+        if p in cache:
+            return cache[p]
         if not p:
-            return True
+            return 1
+        total = 0
         for pref in towels:
             if p.startswith(pref):
-                if dfs(p[len(pref):]):
-                    return True
-        return False
-    return sum(1 for pattern in patterns if dfs(pattern))
-
-def part2():
-    def different_ways(p):
-        cache = {}
-        def dfs(p):
-            if p in cache:
-                return cache[p]
-            if not p:
-                return 1
-            total = 0
-            for pref in towels:
-                if p.startswith(pref):
-                    total += dfs(p[len(pref):])
-            cache[p] = total            
-            return total
-        return dfs(p)
-    return sum(different_ways(pattern) for pattern in patterns)
-  
-print(part1())
-print(part2())
+                total += dfs(p[len(pref):])
+        cache[p] = total            
+        return total
+    return dfs(p)
+print(sum(1 for pattern in patterns if different_ways(pattern)))
+print(sum(different_ways(pattern) for pattern in patterns))
