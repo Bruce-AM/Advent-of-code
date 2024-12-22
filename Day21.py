39ba9af4ab9b88e6a2add700de3368ff6558b8f1
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-22 22:42:27 +0200

    Update Day21.py

--- Day21.py
+++ Day21.py
@@ -66,3 +66,71 @@ for i, code in enumerate(codes):
     a = sequence(code)
     total += a * numcode[i]
 print(total)
+
+#part 2 recursion not working 
+def part2_v1():
+    def sequence(code):
+        shortest = 0
+        r1 = numpad["A"]
+        for num in code:
+            y = r1[0] - numpad[num][0]
+            x = r1[1] - numpad[num][1]
+            if r1[1] == 0 and numpad[num][0] == 3: #y first so we dont hit (3,0)
+                sequence_of_buttons = ("<" * x if x > 0 else ">" * -x) + ("^" * y if y > 0 else "v" * -y) + "A"               
+            elif r1[0] == 3 and numpad[num][1] == 0: #x first so we dont hit (3,0)
+                sequence_of_buttons = ("^" * y if y > 0 else "v" * -y) + ("<" * x if x > 0 else ">" * -x) + "A"       
+            else:
+                if numpad[num][0] > r1[0] and numpad[num][1] > r1[1]:
+                    sequence_of_buttons = ("^" * y if y > 0 else "v" * -y) + ("<" * x if x > 0 else ">" * -x) + "A"
+                elif numpad[num][0] < r1[0] and numpad[num][1] > r1[1]:
+                    sequence_of_buttons = ("^" * y if y > 0 else "v" * -y) + ("<" * x if x > 0 else ">" * -x) + "A"
+                elif numpad[num][0] < r1[0] and numpad[num][1] < r1[1]:
+                    sequence_of_buttons = ("<" * x if x > 0 else ">" * -x) + ("^" * y if y > 0 else "v" * -y) + "A"
+                else:
+                    sequence_of_buttons = ("<" * x if x > 0 else ">" * -x) + ("^" * y if y > 0 else "v" * -y) + "A"
+            r1 = numpad[num]
+
+            @lru_cache(None)
+            def dirrectional_keypads(sequence_of_buttons, hand_pos, depth):
+                if depth == 15:
+                    print(sequence_of_buttons) #at second num shows wrong moves
+                    return len(sequence_of_buttons)
+                last_len = 0
+                for char in sequence_of_buttons:
+                    y = hand_pos[0] - dirpad[char][0]
+                    x = hand_pos[1] - dirpad[char][1]
+                    if hand_pos[1] == 0 and dirpad[char][0] == 0: #we dont hit (0,0)
+                        cur_sequence = ("<" * x if x > 0 else ">" * -x) + ("^" * y if y > 0 else "v" * -y) + "A"
+                    elif hand_pos[0] == 0 and dirpad[char][1] == 0:
+                        cur_sequence = ("^" * y if y > 0 else "v" * -y) + ("<" * x if x > 0 else ">" * -x) + "A"
+                    else:
+                        if dirpad[char][0] > hand_pos[0] and dirpad[char][1] > hand_pos[1]:
+                            cur_sequence = ("^" * y if y > 0 else "v" * -y) + ("<" * x if x > 0 else ">" * -x) + "A"
+                        elif dirpad[char][0] < hand_pos[0] and dirpad[char][1] > hand_pos[1]:
+                            cur_sequence = ("^" * y if y > 0 else "v" * -y) + ("<" * x if x > 0 else ">" * -x) + "A"
+                        elif dirpad[char][0] < hand_pos[0] and dirpad[char][1] < hand_pos[1]:
+                            cur_sequence = ("<" * x if x > 0 else ">" * -x) + ("^" * y if y > 0 else "v" * -y) + "A"
+                        else:
+                            cur_sequence = ("<" * x if x > 0 else ">" * -x) + ("^" * y if y > 0 else "v" * -y) + "A"               
+                    last_len += dirrectional_keypads(cur_sequence, hand_pos, depth + 1)
+                    hand_pos = dirpad[char]
+                    
+                return last_len
+
+            shortest += dirrectional_keypads(sequence_of_buttons, dirpad["A"], 0)
+
+        print(shortest)
+        return shortest
+
+total = 0
+for i, code in enumerate(codes[:1]): # only first number for test
+    a = sequence(code)
+    total += a * numcode[i]
+print(total)
+
+
+
+
+
+
+
