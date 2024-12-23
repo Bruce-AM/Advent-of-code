b65d3bd21de8ead31e5c713e27c5feddab04b707
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-23 15:38:58 +0200

    Update Day23.py

--- Day23.py
+++ Day23.py
@@ -16,14 +16,14 @@ def both():
     longest = []
     for k, values in list_connections.items():
         for i in range(12):
-            cur_lst = [k, values[i]]
+            cur_set = {k, values[i]}
             for j in range(i+1, 13):
                 if values[j] in set_connections[values[i]]: #part1
                     three_computers.add(tuple(sorted([k, values[i], values[j]])))
-                if all(values[j] in set_connections[comp] for comp in cur_lst):
-                    cur_lst.append(values[j])
-            if len(cur_lst) > len(longest): #part2
-                longest = cur_lst   
+                if cur_set.issubset(set_connections[values[j]]):
+                    cur_set.add(values[j])
+            if len(cur_set) > len(longest): #part2
+                longest = cur_set   
     print(sum(1 for tup in three_computers if any(char[0] == "t" for char in tup)))
     print(','.join(sorted(longest)))
 both()
