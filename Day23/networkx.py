18ffa843481a0d1be6c0f848cc22c9e772d06165
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-23 21:30:58 +0200

    Update networkx.py

--- Day23/networkx.py
+++ Day23/networkx.py
@@ -6,6 +6,6 @@ with open("adventofcode/day23/day23_input.txt", "r") as file:
     G.add_edges_from([tuple(line.split("-")) for line in file.read().split()])
     
 cliques = sorted(nx.find_cliques(G), key = len)
-triplets = {tuple(sorted(comb)) for lst in cliques if len(lst) > 2 for comb in combinations(lst, 3)}
+triplets = {tuple(sorted(comb)) for lst in cliques for comb in combinations(lst, 3)}
 print(sum(1 for tup in triplets if any(char[0] == "t" for char in tup))) #part1
 print(sorted(cliques[-1])) #part2
