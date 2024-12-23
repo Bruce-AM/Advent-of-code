361636e016ebfa5c51efd6d0fc1e7de255de42ef
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-23 18:24:50 +0200

    Update Day23.py

--- Day23.py
+++ Day23.py
@@ -11,19 +11,17 @@ with open("day23_input.txt", "r") as file:
         set_connections[pc1].add(pc2)
         set_connections[pc2].add(pc1)
 
-def both():
-    three_computers = set()
-    longest = []
-    for k, values in list_connections.items():
-        for i in range(12):
-            cur_set = {k, values[i]}
-            for j in range(i+1, 13):
-                if values[j] in set_connections[values[i]]: #part1
-                    three_computers.add(tuple(sorted([k, values[i], values[j]])))
-                if cur_set.issubset(set_connections[values[j]]):
-                    cur_set.add(values[j])
-            if len(cur_set) > len(longest): #part2
-                longest = cur_set   
-    print(sum(1 for tup in three_computers if any(char[0] == "t" for char in tup)))
-    print(','.join(sorted(longest)))
-both()
+three_computers = set()
+longest = []
+for k, values in list_connections.items():
+    for i in range(12):
+        cur_set = {k, values[i]}
+        for j in range(i+1, 13):
+            if values[j] in set_connections[values[i]]: #part1
+                three_computers.add(tuple(sorted([k, values[i], values[j]])))
+            if cur_set.issubset(set_connections[values[j]]):
+                cur_set.add(values[j])
+        if len(cur_set) > len(longest): #part2
+            longest = cur_set
+print(sum(1 for tup in three_computers if any(char[0] == "t" for char in tup)))
+print(','.join(sorted(longest)))
