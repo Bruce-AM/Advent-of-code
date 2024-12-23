593bc7f649832a98020ae4339d313e951d7e9a42
Bruce <82039722+Bruce-AM@users.noreply.github.com>
2024-12-23 21:12:00 +0200

    Update networkx.py

--- Day23/networkx.py
+++ Day23/networkx.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from itertools import combinations
 
 G = nx.Graph()
 with open("adventofcode/day23/day23_input.txt", "r") as file:
@@ -6,3 +7,6 @@ with open("adventofcode/day23/day23_input.txt", "r") as file:
 
 cliques = sorted(nx.find_cliques(G), key = len)
 print(sorted(cliques[-1])) #part2
+
+triplets = {tuple(sorted(comb)) for lst in cliques if len(lst) > 2 for comb in combinations(lst, 3)}
+print(sum(1 for tup in triplets if any(char[0] == "t" for char in tup))) #part1
